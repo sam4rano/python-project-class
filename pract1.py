7572c6089482e928f3f140131d85a8cbf8bc225c
Oyerinde Samuel <oyerindesamuelabiodun@gmail.com>
2022-08-12 13:41:44 +0100

    first commit

---
+++ pract1.py
@@ -0,0 +1,16 @@
+
+#largest number
+# largest = -1
+# numlist = [32,6,9,1,10]
+
+# for i in numlist:
+#     if i > largest:
+#         largest = i
+#     print(largest,i)
+# print('the largest', largest)
+
+
+word = "bananana"
+i = word.find("na")
+
+print(i)
